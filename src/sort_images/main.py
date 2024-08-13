52c7b0cff63b3a58a139b1f0cd8c9e598bb396ff
Rudolph Almeida <rudolf1.almeida@gmail.com>
2024-08-13 22:09:03 +1000

    Add pretty prompt and tree

--- src/sort_images/main.py
+++ src/sort_images/main.py
@@ -1,15 +1,18 @@
 import re
 import shutil
-from argparse import ArgumentParser, Action
+from argparse import ArgumentParser
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Generator
 
+from rich import print
+from rich.prompt import Prompt
+from rich.tree import Tree
 
 YEAR_REGEX = re.compile(r"(20\d\d)")
 
 
-@dataclass
+@dataclass(frozen=True)
 class Options:
     directory: Path
     dry_run: bool = field(default=True)
@@ -33,17 +36,14 @@ def parse_options() -> Options:
 
 def misplaced_files_in_directory(path: Path) -> Generator[MisplacedFile, None, None]:
     assert path.is_dir()
-    print(f"Checking folder {path}")
 
     folder_match = re.findall(YEAR_REGEX, path.name)
     if not folder_match:
         return
     folder_year = folder_match[0]
-    print(f"Source folder has the year {folder_year}")
 
     for child in path.iterdir():
         if child.is_dir():
-            print(f"Unknown directory {child} under {path}")
             continue
 
         filename = child.name
@@ -56,28 +56,43 @@ def misplaced_files_in_directory(path: Path) -> Generator[MisplacedFile, None, N
             continue
 
         if len(file_matches) > 1:
-            print(f"Found multiple year matches for file {filename}")
-            continue
+            file_year = Prompt.ask(f"Choose year for {filename}", choices=file_matches)
+        else:
+            file_year = file_matches[0]
 
-        if file_matches[0] != folder_year:
-            yield MisplacedFile(original_path=child, year_from_filename=file_matches[0])
+        if file_year != folder_year:
+            yield MisplacedFile(original_path=child, year_from_filename=file_year)
 
 
 def main() -> None:
     options = parse_options()
+
+    tree = Tree(options.directory.name)
 
     dirs_by_year = {
         re.findall(YEAR_REGEX, dir_path.name)[0]: dir_path
         for dir_path in options.directory.iterdir()
     }
 
+    misplaced_files = []
+
     for child in options.directory.iterdir():
         if not child.is_dir():
             continue
 
+        child_tree = tree.add(child.name)
+
         for misplaced_file in misplaced_files_in_directory(child):
-            if options.dry_run:
-                print(f"Moving file {misplaced_file.original_path.name} from {misplaced_file.original_path.parent} to {dirs_by_year[misplaced_file.year_from_filename]}")
-            else:
-                new_path = dirs_by_year[misplaced_file.year_from_filename] / misplaced_file.original_path.name
-                shutil.move(misplaced_file.original_path, new_path)
+            child_tree.add(misplaced_file.original_path.name)
+            misplaced_files.append(misplaced_file)
+
+    print("Possibly misplaced files")
+    print(tree)
+
+    if options.dry_run:
+        print("Rerun with `--no-dry-run` to make the changes")
+        return
+
+    for misplaced_file in misplaced_files:
+        new_path = dirs_by_year[misplaced_file.year_from_filename] / misplaced_file.original_path.name
+        shutil.move(misplaced_file.original_path, new_path)
